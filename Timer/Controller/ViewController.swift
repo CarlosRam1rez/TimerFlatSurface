b5b6fadd393942bf28f91c0cac4b8b5f8ad5ae21
Carlos Ram <carlosr2-d2@hotmail.com>
2023-07-18 15:54:52 -0600

    Added new validation to handle button status and checked functionality

--- Timer/Controller/ViewController.swift
+++ Timer/Controller/ViewController.swift
@@ -20,7 +20,7 @@ class TimerController: UIViewController {
     
     var counterSec: Int = 0
     var counterMin: Int = 0
-    var counterHour: Int = 2
+    var counterHour: Int = 1
     var rotationRate: Double = 0.0
     
     var manager = CMMotionManager()
@@ -29,8 +29,7 @@ class TimerController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        startButton.addTarget(self, action: #selector(startTimer), for: .touchUpInside)
-        stopButton.addTarget(self, action: #selector(stopTimer), for: .touchUpInside)
+        setupButtons()
         manager.startDeviceMotionUpdates(to: OperationQueue()) { (data: CMDeviceMotion?, error: Error?) in
             guard let data = data else {
                 print("Error: \(error!)")
@@ -43,45 +42,92 @@ class TimerController: UIViewController {
     }
 
     @objc func stopTimer() {
-        timer.invalidate()
+        
         startButton.isHidden = false
-        stopButton.isHidden = true
+        stopButton.isHidden = false
         messageLbl.isHidden = true
+        
+        switch stopButton.tag {
+        case TimerModel().statusPauseButton:
+            timer.invalidate()
+            stopButton.setAttributedTitle(TimerModel().continueTimer, for: .normal)
+            stopButton.tag = TimerModel().statusContinueButton
+        case TimerModel().statusContinueButton:
+            stopButton.setAttributedTitle(TimerModel().pauseTimer, for: .normal)
+            stopButton.tag = TimerModel().statusPauseButton
+            startCounters()
+        case TimerModel().statusCancelButton:
+            resetCounters()
+        default:
+            break
+            
+        }
         
     }
     
     @objc func startTimer() {
-        resetCounters()
-        messageLbl.isHidden = false
-        startButton.isHidden = true
-        stopButton.isHidden = false
-        messageLbl.text = TimerModel().waitMessage
-        timerRotation = Timer.scheduledTimer(withTimeInterval: 2, repeats: true) { _ in
+        switch startButton.tag {
+        case TimerModel().statusCancelButton:
+            resetCounters()
+        default:
+            messageLbl.isHidden = false
+            startButton.isHidden = true
+            startButton.tag = TimerModel().statusCancelButton
+            stopButton.isHidden = false
+            stopButton.setAttributedTitle(TimerModel().cancelTimer, for: .normal)
+            stopButton.backgroundColor = .red
+            stopButton.tag = TimerModel().statusCancelButton
+            messageLbl.text = TimerModel().waitMessage
+            checkRotationUpdate()
+        }
+    }
+    
+    private func checkRotationUpdate() {
+        timerRotation = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { _ in
             
             let motion: CMAttitude = self.deviceMotionData.attitude
             DispatchQueue.main.async {
                 let motionPitchDegrees = motion.pitch * 180 / .pi
                 switch motionPitchDegrees {
-                case 0 ... 0.45:
+                case  0.1 ... 2.0:
                     print("success")
                     print(motionPitchDegrees)
                     self.startCounters()
                     self.timerRotation.invalidate()
                 default:
                     print("failure")
+                    print(motionPitchDegrees)
                 }
             }
         }
     }
     
     private func resetCounters() {
-        timerLbl.text = "\(String(format: "%02d : %02d : %02d", self.counterHour, self.counterMin, self.counterSec))"
+        counterMin = 0
+        counterSec = 0
+        counterHour = 1
+        timerLbl.text = "\(String(format: "%02d : %02d : %02d", counterHour, counterMin, counterSec))"
+        startButton.setAttributedTitle(TimerModel().startTimer, for: .normal)
+        startButton.backgroundColor = .systemOrange
+        timerRotation.invalidate()
+        timer.invalidate()
+        stopButton.isHidden = true
+        stopButton.tag = TimerModel().statusPauseButton
+        startButton.tag = TimerModel().statusPauseButton
     }
     
     private func startCounters() {
         messageLbl.isHidden = true
-        self.counterSec = 59
-        self.counterMin = 59
+        startButton.isHidden = false
+        startButton.setAttributedTitle(TimerModel().cancelTimer, for: .normal)
+        startButton.backgroundColor = .red
+        stopButton.setAttributedTitle(TimerModel().pauseTimer, for: .normal)
+        stopButton.backgroundColor = .systemOrange
+        stopButton.tag = TimerModel().statusPauseButton
+        if counterMin == 0 && counterSec == 0 {
+            self.counterSec = 59
+            self.counterMin = 59
+        }
         timer = Timer.scheduledTimer(withTimeInterval: 0.00000005, repeats: true) { _ in
             self.counterSec -= 1
             if self.counterSec >= 0 {
@@ -95,7 +141,20 @@ class TimerController: UIViewController {
                 self.counterMin = 59
                 self.timerLbl.text = "\(String(format: "%02d : %02d : %02d", self.counterHour, self.counterMin, self.counterSec))"
             }
+            
+            if self.counterMin == 0 && self.counterSec == 0 && self.counterHour == 0 {
+                //HERE sound and vibrate behaivor
+                print("FINISHED!!!")
+            }
           }
+        
+    }
+    
+    private func setupButtons() {
+        startButton.addTarget(self, action: #selector(startTimer), for: .touchUpInside)
+        stopButton.addTarget(self, action: #selector(stopTimer), for: .touchUpInside)
+        startButton.layer.cornerRadius = 20
+        stopButton.layer.cornerRadius = 20
     }
 }
 
--- Timer/Model/TimerModel.swift
+++ Timer/Model/TimerModel.swift
@@ -6,8 +6,16 @@
 //
 
 import Foundation
+import UIKit
 
 struct TimerModel {
     
     let waitMessage: String = "Configurando \nEsperando para iniciar... "
+    let continueTimer: NSAttributedString =  NSAttributedString(string: "Reaunudar", attributes: [NSAttributedString.Key.font: UIFont.systemFont(ofSize: 43)])
+    let cancelTimer: NSAttributedString =  NSAttributedString(string: "Cancelar", attributes: [NSAttributedString.Key.font: UIFont.systemFont(ofSize: 43)])
+    let pauseTimer: NSAttributedString =  NSAttributedString(string: "Pausa", attributes: [NSAttributedString.Key.font: UIFont.systemFont(ofSize: 43)])
+    let startTimer: NSAttributedString =  NSAttributedString(string: "Start", attributes: [NSAttributedString.Key.font: UIFont.systemFont(ofSize: 43)])
+    let statusPauseButton: Int = 0
+    let statusContinueButton: Int = 1
+    let statusCancelButton: Int = 2
 }
